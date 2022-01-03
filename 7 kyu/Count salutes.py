cc1040030d5836a150008a4f1139e01f8c95c95a
Rimvydas Skališius <78035439+RimvaS@users.noreply.github.com>
2022-01-03 17:16:58 +0200

    Delete Count salutes.py

--- 7 kyu/Count salutes.py
+++
@@ -1,9 +0,0 @@
-def count_salutes(hallway):
-    b = hallway.replace('-', '')
-    b = list(b)
-    rez = 0
-    for x in range(0,len(b)-1):
-        for i in range(1,len(b)):
-            if b[x] + b[i] == '><' and i > x:
-                rez += 2
-    return rez
